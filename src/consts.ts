195ceccf000ec114e5000397af4b6bac850a2c48
Arthur Lorhan <arthurlohransilva@gmail.com>
2023-11-11 21:52:40 -0300

    nem sei

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Before the Frustum';
-export const SITE_DESCRIPTION = 'Arthur\'s adventures in graphics programming';
+export const SITE_DESCRIPTION = 'adventures in <s>graphics</s> programming';
 export const SITE_MAX_WIDTH = '720px';
