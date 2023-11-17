0c5f437b2150638012224e2df34e81d37288386f
Arthur Lorhan <arthurlohransilva@gmail.com>
2023-11-17 00:36:12 -0300

    first final design, index page ok

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
+        preview: z.string().min(15),
         // Transform string to Date object
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
