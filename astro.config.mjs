5e6ca88ddbd6b9b6fcb81ac9e24512e4b6021d60
Arthur Lorhan <arthurlohransilva@gmail.com>
2023-11-20 11:19:20 -0300

    more progress on the first article, some design changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+// import gruvboxTheme from "./gruvbox-dark-hard.json";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://nihl999.github.io",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "poimandres",
+    },
+  },
 });
